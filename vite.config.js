56b257faae8bac86495dea00f6a6d314c57719a5
asnawiesapra <asnawiesapra.202300285@gmail.com>
2026-09-15 15:40:08 +0000

    dfdf

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isCodespaces = process.env.CODESPACES === "true";
+
 export default defineConfig({
   plugins: [react()],
+  server: isCodespaces
+    ? {
+        hmr: {
+          clientPort: 443,
+        },
+      }
+    : undefined,
 });
